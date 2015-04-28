722985e368f4525e0c74089062ae8a251fd1c5cc
Matthew Rowe <mlr28@students.calvin.edu>
2015-04-28 09:52:44 -0400

    Built an additional project to store my learning work, can be used as the main project

---
+++ Learning Project/Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGeneration : MonoBehaviour {
+
+	public Transform xpassage;
+	public Transform ypassage;
+	public Transform zpassage;
+	// Use this for initialization
+	void Start () {
+		//x directional passages
+
+		for (int x = 0; x < 4; x++) {
+			for (int y = 0; y < 5; y++) {
+				for (int z =0 ; z < 5; z++) {
+					Instantiate(xpassage, new Vector3(18*x+9,18*y,18*z), Quaternion.identity);
+				}
+			}
+		}
+
+		//y directional passages
+		for (int x = 0; x < 5; x++) {
+			for (int y = 0; y < 4; y++) {
+				for (int z =0 ; z < 5; z++) {
+					Instantiate(ypassage, new Vector3(18*x,18*y+9,18*z), Quaternion.identity);
+				}
+			}
+		}
+
+		//z directional passages
+		for (int x = 0; x < 5; x++) {
+			for (int y = 0; y < 5; y++) {
+				for (int z =0 ; z < 4; z++) {
+					Instantiate(zpassage, new Vector3(18*x,18*y,18*z+9), Quaternion.identity);
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
