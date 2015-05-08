84b303562e8b9b6f8694cd3f599dfa47000f9701
Isaac Zylstra <izylstra@gmail.com>
2015-05-08 14:18:54 -0400

    Added collision detecting cube
    A color changing and teleporting cube! Adding something to do, and an
    example of perhaps a goal. Need to perhaps add solidity.

---
+++ Learning Project/Assets/Scripts/CubeControlScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeControlScript : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player") {
+			TurnColor ();
+			Teleport ();
+		}
+	}
+
+	void TurnColor(){
+		Color col = new Color(Random.value, Random.value, Random.value);
+		GetComponent<Renderer>().material.color = col;
+	}
+
+	void Teleport(){
+		Vector3 pos = transform.position;
+		pos.z = transform.position.z + 5;
+		pos.x = (float)(4 * (Random.value - 0.5));
+		transform.position = pos;
+	}
+
+
+}
