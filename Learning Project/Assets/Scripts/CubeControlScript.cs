4f886fbc02ba35b8bbd761728499c4d17b682653
Isaac Zylstra <izylstra@gmail.com>
2015-05-10 22:54:47 -0400

    Added a completion counter
    Created a GameController Script and modified the CubeController Script.

--- Learning Project/Assets/Scripts/CubeControlScript.cs
+++ Learning Project/Assets/Scripts/CubeControlScript.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class CubeControlScript : MonoBehaviour {
 
+	private GameController gameController;
+
+	void Start(){
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		gameController = gameControllerObject.GetComponent <GameController>();
+	}
 
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "Player") {
-			TurnColor ();
-			Teleport ();
+			gameController.incrementCompletion();
+			Destroy (gameObject);
+			//TurnColor ();
+			//Teleport ();
 		}
 	}
 
+	/*
 	void TurnColor(){
 		Color col = new Color(Random.value, Random.value, Random.value);
 		GetComponent<Renderer>().material.color = col;
@@ -22,6 +31,6 @@ public class CubeControlScript : MonoBehaviour {
 		pos.x = (float)(4 * (Random.value - 0.5));
 		transform.position = pos;
 	}
-
+	*/
 
 }
---
+++ Learning Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+
+	public GUIText completionText;
+	private int completion;
+
+	void Start () {
+		completion = 0;
+		updateCompletion (); 
+	}
+
+	public void incrementCompletion ()
+	{
+		completion++;
+		updateCompletion ();
+	}
+	
+	void updateCompletion ()
+	{
+		completionText.text = "Cubes found: " + completion + "/ 5";
+	}
+
+}
